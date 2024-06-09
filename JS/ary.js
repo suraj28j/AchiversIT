fde70ec9828fce80d1d027a24a003a5588818f6b
Suraj Kumar <suraj28j@gmail.com>
2024-06-09 13:25:14 +0530

    ary

--- JS/ary.js
+++ JS/ary.js
@@ -1,6 +1,39 @@
 let ary1 = [19,35,24,29,16,45,8,13];
 let ary2 = ["Virat","Rohit","Rahul","Hardik","Jasprit","Ravindra"];
 
-// array.sort
-console.log(ary2.sort()); 
-console.log(ary1.sort((a,b)=>a-b));
+// sort() //
+// ary2.sort();
+// console.log(ary2);
+// console.log(ary2.sort()); 
+// ary1.sort((a,b)=>a-b);
+// console.log(ary1);
+// console.log(ary1.sort((a,b)=>a-b));
+
+// reverse() //
+// ary2.reverse();
+// console.log(ary2);
+// console.log(ary1.reverse());
+// console.log(ary2.reverse());
+
+// pop() //
+// ary1.pop();
+// console.log(ary1); 
+// console.log(ary1.pop()); // 13
+
+// push() //
+// ary2.push("Dhoni");
+// console.log(ary2);
+// console.log(ary2.push()); // total elements 6
+// console.log(ary2.push("Dhoni")); // total elements 6+1 = 7
+
+// shift() //
+// ary2.shift();
+// console.log(ary2);
+// console.log(ary2.shift()); // Virat
+
+// unshift() //
+// ary2.unshift("Dhoni");
+// console.log(ary2);
+// console.log(ary2.unshift()); // total elements 6
+// console.log(ary2.unshift("Dhoni"));  // total elements 6+1 = 7
+
