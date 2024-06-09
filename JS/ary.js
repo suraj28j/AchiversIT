48c5c614dfb61be8a01ad9e403a24d18da7efd13
Suraj Kumar <suraj28j@gmail.com>
2024-06-09 13:00:39 +0530

    ary

---
+++ JS/ary.js
@@ -0,0 +1,6 @@
+let ary1 = [19,35,24,29,16,45,8,13];
+let ary2 = ["Virat","Rohit","Rahul","Hardik","Jasprit","Ravindra"];
+
+// array.sort
+console.log(ary2.sort()); 
+console.log(ary1.sort((a,b)=>a-b));
