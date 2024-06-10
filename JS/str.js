a43315cc55c27aa306cb98b844496ddd44d9ccb0
Suraj Kumar <suraj28j@gmail.com>
2024-06-10 18:55:09 +0530

    str

---
+++ JS/str.js
@@ -0,0 +1,91 @@
+let str1 = "Java Script is popular language it's is easy to learn.";
+
+
+// lenghth //
+// console.log(str1.length);
+
+// toLowerCase() //
+// console.log(str1.toLowerCase());
+
+// toUpperCase() //
+// console.log(str1.toUpperCase());
+
+// include() //
+// console.log(str1.includes("easy"));
+
+// search() //
+// console.log(str1.search("easy")); // return index value
+
+// replace() //
+// console.log(str1.replace("is","IS"));
+// console.log(str1.replace(/is/g,"IS"));
+
+// trim() //
+// let str2 = "   Suraj";
+// console.log(str2);
+// console.log(str2.trim());
+
+// charAt() //
+// console.log(str1.charAt(7));
+
+// charCodeAt() //
+// console.log(str1.charCodeAt(7));
+
+// String.fromCharCode() //
+// console.log(String.fromCharCode(65));
+
+// startsWith() //
+// console.log(str1.startsWith("Java"));
+
+// endsWith() //
+// console.log(str1.endsWith("Java"));
+
+// match() //
+// console.log(str1.match(/[A-Z]/g));
+// console.log(str1.match(/is/g));
+
+// indexOf() //
+// console.log(str1.indexOf("is"));
+
+// lastIndexOf() //
+// console.log(str1.lastIndexOf("is"));
+
+// split() //
+// let word = str1.split(" ");
+// console.log(word);
+// let char = str1.split(""); 
+// console.log(char);
+// let word_3 = str1.split(" ",3);
+// console.log(word_3);
+
+
+// concat() //
+// console.log(str1.concat(" and I love Java Script"));
+
+// repeat() //
+// console.log(str1.repeat(3));
+
+// slice() //
+// console.log(str1.slice(5,30));
+// console.log(str1.slice(-6));
+
+// substring() //
+// console.log(str1.substring(5,30));
+// console.log(str1.substring(-6));
+
+// padStart() //
+// let account = "9865"
+// console.log(account.padStart(10,'*'));
+
+// padStart() //
+// let mobile = "8115"
+// console.log(mobile.padEnd(10,'*'));
+
+// String.raw //
+// let filepath = `C:\suraj\new folder`; // MetaCharacters => \n \s \f ...etc
+// let filepath = String.raw `C:\suraj\new folder`
+// console.log(filepath);
+
+// fromCodePoint() //
+// console.log(String.fromCodePoint(9731));
+// console.log(String.fromCodePoint(9733));
