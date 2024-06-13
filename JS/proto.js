d89f29dcd3537711fbf90a7745e74dba36825cd7
Suraj Kumar <suraj28j@gmail.com>
2024-06-13 16:17:04 +0530

    proto

--- JS/proto.js
+++ JS/proto.js
@@ -1,1 +1,16 @@
-// proto
+// Object.prototype
+
+let user = {
+    name:"Suraj",
+    city:"Bangalore",
+}
+
+Object.prototype.msg = function(){
+    console.log(`My name is ${this.name} and I live ${this.city}`);
+} // this mathod is assign Objects and as well as Array
+
+user.msg(); 
+
+
+let ary = ["Suraj","Bangalore"];
+ary.msg();
