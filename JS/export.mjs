5bba53a652235f4314aa85b18193d8ff785770b8
Suraj Kumar <suraj28j@gmail.com>
2024-06-14 12:19:50 +0530

    module

---
+++ JS/export.mjs
@@ -0,0 +1,16 @@
+import {name,user,ary,userName} from "./import.mjs"
+console.log(name);
+console.log(user);
+console.log(ary);
+userName();
+
+
+/*
+import file and export file extantion should be .mjs
+
+in import file 
+useing 'import' keyword before declear the vaiables,functions,arrays,objects
+
+in export file
+using import keyword and mention variables,function,arrays,objects in Curly brace { } from "./fileName.mjs"
+*/
---
+++ JS/import.mjs
@@ -0,0 +1,13 @@
+export let name = "Suraj";
+export let user = {
+        name,
+        age:33,
+        city:"Bangalore"
+}
+// console.log(user);
+
+
+export let ary = ["Sai",30,"Banglaore"];
+export function userName(){
+    console.log(`My name is ${name}`);
+}
