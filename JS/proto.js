5a4c12a11f0b3770341d4ea519b9310501eb9d8d
Suraj Kumar <suraj28j@gmail.com>
2024-06-16 19:12:27 +0530

    proto

--- JS/proto.js
+++ JS/proto.js
@@ -1,15 +1,17 @@
 // Object.prototype //
 
+
+// ...with object literal //
 let user = {
     name:"Suraj",
     city:"Bangalore",
 }
 
-// adding property to constructor //
+// adding property to object //
 // user.age = 33;
 // console.log(user.age);
 
-// adding mathod to constructor //
+// adding mathod to object //
 // user.msg = function(){
 //     console.log(`My name is ${this.name} and I live ${this.city}`);
 // }
@@ -22,7 +24,22 @@ let user = {
 
 // user.msg(); 
 // let ary = ["Suraj","Bangalore"];
-// ary.msg();
+// ary.msg(); // msg() mathod aslo assign to array
 
 
+// ...with constructor function //
+let student = function(name,city){
+    this.name= name,
+    this.city=city,
+    this.wel=function(){
+        console.log(`Welcome ${this.name}`);
+    }
+} 
 
+let rollNo1 = new student("Suraj","Bangalore");
+// rollNo1.wel();
+
+student.prototype.isLogin = function(){
+    console.log(`${this.name} is login`);
+}
+// rollNo1.isLogin();
