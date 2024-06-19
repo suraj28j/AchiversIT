2bf09209e67682c90a96086fce78450a59177885
Suraj Kumar <suraj28j@gmail.com>
2024-06-19 12:31:13 +0530

    fun3

--- JS/fun3.js
+++ JS/fun3.js
@@ -39,3 +39,37 @@ const combFun = (x) => multi(add(x));
 // [Memoization]
 /* it is optimization technique where the result of expensive function calls are cached 
 so that subsequent call with same input return the cached result rather than recalculating the result */
+
+
+// [Destructuring] 
+// it is the process of extracting the elements from the array or properties from the object and store the same as indivisual in a single line
+
+// Example 1 for Array
+// let ary = [10,20,30];
+// let [one,two,three] = ary;
+// console.log(one);
+
+// Example 2 for Array
+// let user = ["Suraj",33,"Banglore"];
+// let [name,age,city] = user
+// console.log(name);
+// console.log(age);
+
+// Example 3 for Object
+// let user = {
+//     name:"Suraj",
+//     age:33,
+//     city:"Bangalore"
+// }
+// let {name,age,city} = user;
+// console.log(name);
+
+// Example 4 for Object
+// let user = {
+//     name:"Suraj",
+//     age:33,
+//     city:"Bangalore"
+// }
+// let {name:userName,age:Age,city:City} = user;
+// console.log(userName);
+// console.log(Age);
