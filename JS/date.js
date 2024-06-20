d6e9396939f6af1e44dc3cb0c12b613812d86be0
Suraj Kumar <suraj28j@gmail.com>
2024-06-20 22:55:12 +0530

    date

---
+++ JS/date.js
@@ -0,0 +1,27 @@
+let now = new Date();
+
+// [get] //
+// console.log(now.toDateString()); // Thu Jun 20 2024
+// console.log(now.getDate());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+// console.log(now.getHours());
+
+// function formate() {
+//     let hours = now.getHours();
+//     let ampm = hours>=12 ? "PM" : "AM";
+//     hours = hours%12;
+//     return `${hours} ${ampm}`
+// }
+// console.log(formate());
+
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+// console.log(now.getMilliseconds());
+
+
+// [set] //
+
+let date = 
+console.log(now.setFullYear(2022));
