63ff895937bcb06f6912643a460e26b45d604c4a
Suraj Kumar <suraj28j@gmail.com>
2024-06-24 16:17:38 +0530

    update

--- projects/achivers it/assets/js/index.js
+++ projects/achivers it/assets/js/index.js
@@ -1,1 +1,13 @@
-let banneImg = document.querySelector(".banner");
+// for header ul
+// for media 768px
+let count = 0;
+function listShow() {
+    let ul = document.getElementById("ful");
+    if (count % 2 == 0) {
+        ul.style.display = 'block';
+        count++;
+    } else {
+        ul.style.display = 'none';
+        count--;
+    }
+}
