4410d1b0b7d04c9fecd9045ee4aa54820aa6aabc
Suraj Kumar <suraj28j@gmail.com>
2024-07-19 23:29:10 +0530

    update

--- JS/dest.js
+++ JS/dest.js
@@ -1,29 +1,35 @@
+//Destructuring
+// it is the process of extracting the elements from the array or properties from the object and store the same as indivisual in a single line
+
+
+
+
 // Array 
 
-let numbers = [23,24,25,26,27,28];
-let[num1,num2,num3,num4,num5,num6] = numbers;
+let numbers = [23, 24, 25, 26, 27, 28];
+let [num1, num2, num3, num4, num5, num6] = numbers;
 console.log(num6);
 
-let user = ["Suraj",33,"Bangalore"];
-let[name,age,city] = user;
+let user = ["Suraj", 33, "Bangalore"];
+let [name, age, city] = user;
 console.log(name);
 
 
 // Object
 
 let student = {
-    std_name:"Ram Singh",
-    roll_No:101,
-    mark:70,
+    std_name: "Ram Singh",
+    roll_No: 101,
+    mark: 70,
 }
-let {std_name,roll_No,mark} = student;
+let { std_name, roll_No, mark } = student;
 console.log(std_name);
 
 let employee = {
-    id:101,
-    emp_name:"Suraj",
-    salary:35000,
+    id: 101,
+    emp_name: "Suraj",
+    salary: 35000,
 }
-let {id,emp_name:empName,salary} = employee;
+let { id, emp_name: empName, salary } = employee;
 console.log(empName);
 
