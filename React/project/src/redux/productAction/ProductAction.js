1e310c57b5f14fef5aa56e0efb6fa6b609612d58
Suraj Kumar <suraj28j@gmail.com>
2024-08-26 00:25:21 +0530

    quantity set

--- React/project/src/redux/productAction/ProductAction.js
+++ React/project/src/redux/productAction/ProductAction.js
@@ -6,3 +6,7 @@ export const addToCart = (product) => {
 export const removeFromCart = (id) => {
     return {type:ACTION_TYPE.REMOVE_FROM_CART,payload:id}
 }
+
+export const setDecrement = (id) => {
+    return {type:ACTION_TYPE.SET_DECREMENT,payload:id}
+}
--- React/project/src/redux/productReducer/ProductReducer.js
+++ React/project/src/redux/productReducer/ProductReducer.js
@@ -1,22 +1,43 @@
 const initState = {
-    cartData : [],
-    products : []
+    cartData: [],
+    products: []
 }
 
-export const productReducer = (state=initState,action) => {
-        // console.log(action);        
-        switch(action.type){
-            case 'ADD_TO_CART' :
-                    return{
-                        ...state,cartData:[...state.cartData,action.payload]
-                    }
-            case 'REMOVE_FROM_CART' :
-                let filterItem = state.cartData.filter((item)=>{
-                    return  item.id !== action.payload
-                })
+export const productReducer = (state = initState, action) => {
+    // console.log(action);        
+    switch (action.type) {
+        case 'ADD_TO_CART':
+            let findItem = state.cartData.find((item) => {
+                return item.id === action.payload.id
+            })
+            if (findItem) {
+                findItem.quantity = findItem.quantity + 1;
                 return {
-                    ...state,cartData:filterItem
+                    ...state, cartData: [...state.cartData]
                 }
-            default :return state
-        }
+            } else {
+                action.payload.quantity = 1;
+                return {
+                    ...state, cartData: [...state.cartData, action.payload]
+                }
+            }
+
+        case 'REMOVE_FROM_CART':
+            let filterItem = state.cartData.filter((item) => {
+                return item.id !== action.payload
+            })
+            return {
+                ...state, cartData: filterItem
+            }
+
+        case 'SET_DECREMENT':
+            let itemForDecrement = state.cartData.find((item) => {
+                return item.id === action.payload;
+            })
+            itemForDecrement.quantity = itemForDecrement.quantity > 1 ? itemForDecrement.quantity - 1 : 1;
+            return {
+                ...state, cartData: [...state.cartData]
+            }
+        default: return state
+    }
 }
