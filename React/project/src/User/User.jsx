9a8dde1f2c6bb2e9ddd16fca0b7d7920698844bd
Suraj Kumar <suraj28j@gmail.com>
2024-08-26 00:57:44 +0530

    login page

--- React/project/src/User/User.jsx
+++ React/project/src/User/User.jsx
@@ -1,9 +1,33 @@
+import logo from '../Images/logo.png'
 import React from 'react'
 
 const User = () => {
   return (
-    <div>
-      <h2>Welcome User</h2>
+    <div className='container'>
+      <div className='row mt-4 mb-4 text-center'>
+        <div className='col-md-4'></div>
+
+        <div className='col-md-4 mt-4 mb-4 border '>
+        <div className='row mt-2'>
+        <img src={logo} alt='logo' className='img-fluid'></img>
+        </div>
+        <div className='row mt-4 mb-4 d-flex justify-content-center'>
+          <h3>USER LOGIN</h3>
+          <form className='w-75'>
+          <div class="input-group mt-4">
+            <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill"></i></span>
+            <input type='text' className='form-control' aria-label="Username" aria-describedby="basic-addon1"/>
+          </div>
+          <div class="input-group mt-4">
+            <span class="input-group-text" id="basic-addon1"><i class="bi bi-lock-fill"></i></span>
+            <input type='text' className='form-control' aria-label="Password" aria-describedby="basic-addon1"/>
+          </div>
+          </form>
+        </div>
+        </div>
+
+        <div className='col-md-4'></div>
+      </div>
     </div>
   )
 }
