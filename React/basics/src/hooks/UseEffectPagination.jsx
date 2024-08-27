bf7037782a99b339951be6f2333e25ddf4e5d94d
Suraj Kumar <suraj28j@gmail.com>
2024-08-27 22:57:58 +0530

    pagination

--- React/basics/src/hooks/UseEffectPagination.jsx
+++ React/basics/src/hooks/UseEffectPagination.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import key from './key'
+import { key } from './key'
+
 
 const UseEffectPagination = () => {
     const [user, setUser] = useState([])
@@ -10,39 +11,64 @@ const UseEffectPagination = () => {
     }, [page]);
 
     let getData = async () => {
-        let data = await fetch(`https://dummyapi.io/data/v1/user?${page}&limit=10`,{
-            method:'GET',
-            headers:{
-                'api-id':key
-            }
-        });
+        let data = await fetch(`https://dummyapi.io/data/v1/user?page=${page}&limit=10`,
+            {
+                method: 'GET',
+                headers: {
+                    "app-id": key,
+                }
+            });
         let res = await data.json();
-        setUser(res)
+        console.log(res.data);
+        setUser(res.data)
+    }
+
+    const buttonHandler = (pageNo) => {
+        setPage(pageNo);
     }
     return (
         <div className='container'>
             <div className='row'>
                 <h3 className='mt-2 text-center'>Pagination</h3>
                 <div className='col-md-3'></div>
-                <div className='col-md-6'>
+                <div className='col-md-6 mb-4'>
                     {
-                        user.map((item)=>(
-                            <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col">Picture</th>
-                                <th scope="col">Id</th>
-                                <th scope="col">Name</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{}</td>
-                                <td>{}</td>
-                                <td>{}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                        user.length ? (
+                            <div className='table-responsive'>
+                                <table className="table table-border">
+                                    <thead className="thead-light">
+                                        <tr>
+                                            <th scope="col" className="border">Picture</th>
+                                            <th scope="col" className="border">Id</th>
+                                            <th scope="col" className="border">Name</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            user.map((item) => (
+                                                <tr key={item.id}>
+                                                    <td className="border">
+                                                        <img src={item.picture} alt={item.id} className='img-fluid border' />
+                                                    </td>
+                                                    <td className="border">{item.id}</td>
+                                                    <td className="border">
+                                                        {item.title} {item.firstName} {item.lastName}
+                                                    </td>
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
+                        ) : (
+                            <div className='mt-4 mb-4 text-center'>
+                                <h2>Loading...</h2>
+                            </div>
+                        )
+                    }
+                    {
+                        [1, 2, 3, 4, 5, 6].map((num) => (
+                            <button className='btn btn-primary ms-2' onClick={() => buttonHandler(num)}>{num}</button>
                         ))
                     }
                 </div>
