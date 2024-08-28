9ef548cc42f7c286492d83e9f8acc6e5b77f1fc8
Suraj Kumar <suraj28j@gmail.com>
2024-08-28 22:42:58 +0530

    task 2

--- Node/basics/app.js
+++ Node/basics/app.js
@@ -2,9 +2,15 @@ const http = require('http');
 
 function requestListnerCb(req,res){
     console.log("Server is runing");
-    process.exit()
-    // console.log(req);
-    
+    res.setHeader("Content-Type","text/html");
+    res.write("<html>")
+    res.write("<head><title>My Profile</title></head>")
+    res.write("<h1>Hello ! my name is Suraj</h1>");
+    res.write("<p>Learning Node JS</p>")
+    res.write("<input type='text'><br>")
+    res.write("<button>Submit</botton>")
+    res.write("</html>")
+    res.end();
 }
 const server = http.createServer(requestListnerCb);
 server.listen(3000)
