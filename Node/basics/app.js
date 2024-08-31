24265c9c7e60a99df6b18b84b310aef8c0a5d085
Suraj Kumar <suraj28j@gmail.com>
2024-08-31 17:54:52 +0530

    update

--- Node/basics/app.js
+++ Node/basics/app.js
@@ -1,5 +1,7 @@
 const http = require('http');
-const fs = require('fs')
+const routes = require('./routes')
+console.log(routes);
+
 
 
 // function requestListnerCb(req,res){
@@ -25,60 +27,6 @@ const fs = require('fs')
 // }
 // requestListnerCb is a plan CB function which is going to take request and respone as a parameter
 
-const server = http.createServer((req, res) => {
-    // console.log(req);
-    let fileData;
-    if (req.url === "/") {
-        res.write("<html>")
-        res.write("<head><title>Form</title></head>")
-        res.write("<body>")
-        res.write("<form method='POST' action='/message'><input type='text' name = 'message'><button type ='submit'>Submit</botton></form>")
-        res.write("</body>")
-        res.write("</html>")
-        return res.end();
-    }
-    if(req.url === '/message' && req.method === 'POST'){
-
-        let bodyData = [];
-        // let fileData;
-        req.on('data',(chunk)=>{
-            // console.log(chunk);
-            bodyData.push(chunk);
-            console.log(bodyData);
-        })
-        req.on('end',()=>{
-            let parseData = Buffer.concat(bodyData).toString()
-            // console.log(parseData);
-            const message = parseData.split("=")[1]
-            console.log(message);
-            fs.writeFileSync('dummy.txt',message);    
-             fileData = fs.readFileSync('./dummy.txt',{
-                encoding:'utf-8',flag:'r'
-            })
-            console.log(fileData);
-        })
-        res.statusCode = 302;
-        // res.setHeader('Location','/');
-        
-        // res.write("<html>")
-        // res.write("<body>")
-        // res.write("<head><title>My Profile</title></head>")
-        // res.write(`<h1>${fileData}</h1>`);
-        // res.write("</body>")
-        // res.write("</html>")
-        
-        // return res.end();
-    }
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>")
-    res.write("<body>")
-    res.write("<head><title>My Profile</title></head>")
-    res.write(`<h1>${fileData}</h1>`);
-    // res.write("<h1>Hello ! my name is Suraj</h1>");
-    res.write("</body>")
-    res.write("</html>")
-    res.end();
-
-});
+const server = http.createServer(routes);
 
 server.listen(3000)
--- Node/path.js
+++ Node/path.js
@@ -1,5 +1,21 @@
 const path = require('path');
 
+// console.log(path.dirname(__dirname)); //   D:\AchiversIT
+// console.log(path.basename(__dirname)); //  node
+
+// const parse = path.parse(__dirname);
+// console.log(parse);
+/*
+{
+  root: 'D:\\',
+  dir: 'D:\\AchiversIT',
+  base: 'node',
+  ext: '',
+  name: 'node'
+}
+*/
+
+
 // console.log(path.dirname('C:/Users/suraj/Desktop/AchieversIT/Node/modules/path.js'));
 
 // console.log(path.basename('C:/Users/suraj/Desktop/AchieversIT/Node/modules/path.js'));
@@ -10,4 +26,5 @@ const path = require('path');
 
 // let pathName = path.parse('C:/Users/suraj/Desktop/AchieversIT/Node/modules/path.js')
 // console.log(pathName.name);
+
 
