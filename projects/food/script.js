d0e415ede4131afbc446964fd56710810c03f46e
Suraj Kumar <suraj28j@gmail.com>
2024-09-11 13:32:12 +0530

    update

---
+++ projects/food/script.js
@@ -0,0 +1,20 @@
+let cart_data;
+async function foods() {
+    let getData = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+    cart_data = await getData.json();
+    // let {categories} = cart_data;
+    // console.log(cart_data);
+}
+foods();
+
+
+function beefFood(){
+    console.log(cart_data);
+    
+    // let {categories} = cart_data;
+    // let beefItem = categories.filter((item)=>{
+    //     return item.setCategories === 'Beef'
+    // })
+    // console.log(beefItem);
+}
+beefFood()
