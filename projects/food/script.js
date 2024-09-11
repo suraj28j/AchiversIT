04120b5cf6378cf3af520f6acd636fc5b13fe85d
Suraj Kumar <suraj28j@gmail.com>
2024-09-11 15:25:30 +0530

    update

--- projects/food/script.js
+++ projects/food/script.js
@@ -1,20 +1,24 @@
-let cart_data;
-async function foods() {
-    let getData = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
-    cart_data = await getData.json();
-    // let {categories} = cart_data;
-    // console.log(cart_data);
-}
-foods();
+// Not using 
+// testing purpose
 
 
-function beefFood(){
-    console.log(cart_data);
+// let cart_data;
+// async function foods() {
+//     let getData = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+//     cart_data = await getData.json();
+//     // let {categories} = cart_data;
+//     // console.log(cart_data);
+// }
+// foods();
+
+
+// function beefFood(){
+//     console.log(cart_data);
     
-    // let {categories} = cart_data;
-    // let beefItem = categories.filter((item)=>{
-    //     return item.setCategories === 'Beef'
-    // })
-    // console.log(beefItem);
-}
-beefFood()
+//     // let {categories} = cart_data;
+//     // let beefItem = categories.filter((item)=>{
+//     //     return item.setCategories === 'Beef'
+//     // })
+//     // console.log(beefItem);
+// }
+// beefFood()
