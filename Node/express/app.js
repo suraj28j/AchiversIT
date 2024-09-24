63c7682e74370cf5feb98ab89f39c7e96f39ab36
Suraj Kumar <suraj28j@gmail.com>
2024-09-24 22:19:24 +0530

    update

--- Node/express/app.js
+++ Node/express/app.js
@@ -24,7 +24,7 @@ app.use(errorController)
 // mongoose.connect(process.env.MANGO_URI)
 
 mongoose
-    .connect('mongodb+srv://mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .connect('mongodb+srv://suraj28j:testingdb@cluster0.nehjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(console.log('Connection established'),app.listen(3000))
     .catch(err => console.log(err))
     
