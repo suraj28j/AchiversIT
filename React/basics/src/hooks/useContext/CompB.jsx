dcdaf6baa4a53adb50219053fe512f2b75b2ac03
Suraj Kumar <suraj28j@gmail.com>
2024-10-20 20:14:58 +0530

    update

--- React/basics/src/hooks/useContext/CompB.jsx
+++ React/basics/src/hooks/useContext/CompB.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import CompC from './CompC'
+import { courseContext } from './CompA'
 
 const CompB = () => {
+  const myCourse = useContext(courseContext)
   return (
     <div>
       {/* <CompC name={props.name}/> */}
+      <h2>I'm learning {myCourse}</h2>
       <CompC/>
     </div>
   )
