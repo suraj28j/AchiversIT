d56c43cf19e82cabf9e163096076df23162c400b
Suraj Kumar <suraj28j@gmail.com>
2024-11-19 10:29:16 +0530

    update

--- Node/groupChat/app.js
+++ Node/groupChat/app.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static(path.join("./", "public")));
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -14,25 +15,45 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', "login.html"));
 })
-
-app.post('/login', (req, res) => {
-    const user = req.body.user
-    // console.log(user);
-    // fs.writeFile('users',user,(err)=>{
-    //     console.log(err); 
-    // })
-    res.sendFile(path.join(__dirname, 'public', "chat.html"));
-    res.user = user;
+app.get('/message', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'message.html'))
+    // res.write(`
+    //     <html>
+    //         <head>
+    //         </head>
+    //         <body>
+    //          <form>
+    //             <label for="user">Message : </label><br>
+    //             <input type="text" id="message" name="message" oninput="handleChange(this)" required><br>
+    //             <button type="submit">Submit</button>
+    //         </form>
+            
+    //         </body>
+    //     </html>
+    //     `)
 })
 
-app.post('/chat', (req, res) => {
-    const user = req.body.user
-    console.log(user);
-    
-    // fs.writeFile('users',user,(err)=>{
-    //     console.log(err); 
-    // })
-    // res.sendFile(path.join(__dirname, 'public', "chat.html"));
+let dataPath = path.join(path.dirname(process.mainModule.filename), "data", "msg.json");
+
+const getDataFromFile = (cb) => {
+    fs.readFile(dataPath, (err, data) => {
+        if (err) {
+            return cb([])
+        } else {
+            cb(JSON.parse(data));
+        }
+    })
+}
+
+app.post('/message', (req, res) => {
+    // console.log(req.body);
+    getDataFromFile((c) => {
+        userInfo.push(req.body)
+        fs.writeFile(dataPath, JSON.stringify(userInfo), (err) => {
+            console.log(err);
+        });
+    });
+    res.redirect("/message");
 })
 
 app.listen('3000', () => {
